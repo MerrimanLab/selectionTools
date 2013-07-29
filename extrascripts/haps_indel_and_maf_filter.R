5005c7e396b10aef2b20f61ea4b20c857312bd56
Murray Cadzow <murray.cadzow@otago.ac.nz>
2013-07-30 11:20:44 +1200

    fixed error

--- extrascripts/haps_indel_and_maf_filter.R
+++ extrascripts/haps_indel_and_maf_filter.R
@@ -6,7 +6,7 @@
 args<-commandArgs(TRUE)
 #read in haps file from shapeit
 hapsPop=read.table(file=args[1])
-maf=as.numeric(args2)
+maf=as.numeric(args[2])
 af = apply(hapsPop[,6:length(hapsPop[1,])], 1, FUN=function(x){sum(x)/length(x)}  )
 hapsPop=hapsPop[(af > maf) & (af < (1-maf)),]
 print(paste("af >",maf))
