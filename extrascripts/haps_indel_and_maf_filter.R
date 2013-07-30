42b8d983af62d0f4936f9c49452a20d9b5add414
Murray Cadzow <murray.cadzow@otago.ac.nz>
2013-07-30 12:14:50 +1200

    re-added removing indels

--- extrascripts/haps_indel_and_maf_filter.R
+++ extrascripts/haps_indel_and_maf_filter.R
@@ -7,6 +7,15 @@ args<-commandArgs(TRUE)
 #read in haps file from shapeit
 hapsPop=read.table(file=args[1])
 maf=as.numeric(args[2])
+
+hapsPop=read.table(file=args[1])
+hapsPop=hapsPop[nchar(as.character(hapsPop[,4]))==1 &
+                     as.character(hapsPop[,4]) != "-" &
+                     nchar(as.character(hapsPop[,5]))==1 &
+                     as.character(hapsPop[,5]) != "-", ] #remove indels
+hapsPop[,1]= hapsPop[,2]
+
+
 af = apply(hapsPop[,6:length(hapsPop[1,])], 1, FUN=function(x){sum(x)/length(x)}  )
 hapsPop=hapsPop[(af > maf) & (af < (1-maf)),]
 print(paste("af >",maf))
