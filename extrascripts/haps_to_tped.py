8a780b456fa2f3e09aace6585c64c35f814965b3
James Boocock <sfk2001@gmail.com>
2013-08-12 15:23:16 +1200

    Added script to go from haps to tped

---
+++ extrascripts/haps_to_tped.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+#
+# Script for converting from haps to tped
+# 
+# haps to tped.
+#
+import os
+import logging
+import shutil
+from optparse import OptionParser
+
+logging.basicConfig(format="%(asctime)s %(message)s")
+
+log = logging.getLogger(__name__)
+log.setLevel(level=logging.INFO)
+
+def make_allelic_lambda(ref,alt):
+    return lambda x: ref if 0 == int(x) else alt 
+
+def hap_to_tped(basename,output_basename,chromosome):
+    log.debug("Started converting from HAPS to TPED")
+    tped_out = open(output_basename + '.tped','w')
+    # Use shutil to copy sample to tfam
+    shutil.copy(basename + '.sample', output_basename + '.tfam')
+    #default centimorgan values.
+    centimorgans = '0'
+    with open(basename + '.haps','r') as f:
+        log.debug("Read in TPED file")
+        for line in f:
+            split_line = line.split()
+            rsid = split_line[0]
+            pos = split_line[2]
+            ref = split_line[3]
+            alt = split_line[4]
+            allele_lambda = make_allelic_lambda(ref,alt)
+            alleles = map(allele_lambda,split_line[5:])
+            tped_out.write(chromosome + ' ' + rsid + ' ' + centimorgans + ' ' + pos +  ' '.join(alleles) + '\n') 
+    tped_out.close()
+    log.debug("Finished converting from HAPS to TPED")
+
+def main():
+    parser= OptionParser()
+    parser.add_option("-i",'--file',dest="haps_files",help="Base names of haps / sample pair")
+    parser.add_option('-v','--verbose',action="store_true",dest="verbose",help="Verbosity of the logging outputs")
+    parser.add_option('-o','--output',dest="output",help="Output file basename (w+) used so better clean up directory after running program")
+    parser.add_option('-c','--chromosome',dest="chromosome",help="Chromosome haps file originates from")
+    (options,args) = parser.parse_args()
+    if(options.verbose == None):
+        log.setLevel(logging.ERROR)
+    else:
+        log.setLevel(logging.DEBUG)
+    assert (options.haps_files) is not None, "Atleast two haps file base names are required"
+    assert options.output is not None, "Output base name needs to be specified"
+    hap_to_tped(options.haps_files,options.output,options.chromosome)
+
+if __name__=="__main__":main() 
