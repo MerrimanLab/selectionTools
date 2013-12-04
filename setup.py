1398bf5be5401801a66c38fd1a0f36d874b10f4f
James Boocock <sfk2001@gmail.com>
2013-12-04 14:18:41 +1300

    Started using flake8

--- setup.py
+++ setup.py
@@ -5,14 +5,13 @@ setup(
     name="MerrimanSelectionPipeline",
     version="0.3",
     packages=find_packages(),
-            
-    author= "James Boocock",
+    author="James Boocock",
     author_email="smilefreak@gmx.com",
     description="Selection Pipeline for VCF Data",
-    license = "MIT",
+    license="MIT",
     keywords="iHS ehh selection evolution",
     zip_safe=False,
-    entry_points ={
+    entry_points={
         'console_scripts': [
             'ancestral_annotation = selection_pipeline.aa_annotate:main',
             'selection_pipeline = selection_pipeline.fullprocess:main',
