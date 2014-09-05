3ff9a3c03a17895ee6fade836a5f33cb2aaf88ac
James Boocock <sfk2001@gmail.com>
2014-09-05 14:11:12 +1200

    Update run_pipeline.py
    Remove comma

--- selection_pipeline/run_pipeline.py
+++ selection_pipeline/run_pipeline.py
@@ -96,7 +96,7 @@ class CommandTemplate(object):
             if fnmatch.fnmatch(
                     file, self.config['genetic_map']['genetic_map_prefix'].replace(
                         '?', self.options.chromosome)):
-                        genetic_map = file,
+                        genetic_map = file
         assert genetic_map is not None, \
                 "Cannot find genetic map for chromosome {0}, check config".format(self.options.chromosome)
         genetic_map=os.path.join(self.config['genetic_map']['genetic_map_dir'], genetic_map)
