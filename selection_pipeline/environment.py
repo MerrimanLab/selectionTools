44951fb2b20f039fd44ba0611424da414d10f1d7
Murray Cadzow <murray.cadzow@otago.ac.nz>
2014-10-09 16:24:18 +1300

    fixed LD_LIBRARY_PATH automatic replace

--- selection_pipeline/environment.py
+++ selection_pipeline/environment.py
@@ -10,8 +10,9 @@ def set_environment(environment_variables):
     for environ, value in environment_variables.items():
         environ = environ.upper()
         if(environ in os.environ):
-            os.environ[environ] += (value)
-        os.environ[environ] = value
+            os.environ[environ] += (":" + value)
+        else:
+            os.environ[environ] = value
 
 
 
