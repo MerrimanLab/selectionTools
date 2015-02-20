8d9f0efc0cf8b22a496449c78c3c42a6aab11c90
Murray Cadzow <murray.cadzow@otago.ac.nz>
2015-02-20 13:58:49 +1300

    completed TODO list and made nicer

--- extrascripts/kaks.py
+++ extrascripts/kaks.py
@@ -1,17 +1,43 @@
 import vcf
-file = "mis_syn.txt"
+import sys
+
+# HOW TO RUN:
+# use snpEff to annoate VCF file
+# grep '^#\|missense_variant\|synonymous_variant' annotated_vcf.vcf > mis_syn.txt 
+# python kaks.py mis_syn.txt > kaks.txt
+
+
+
+file = sys.argv[1]
 vcf_reader = vcf.Reader(open(file, 'r'))
 gene_name_index = vcf_reader.infos['ANN'][3].split("|").index(" Gene_Name ")
 geneid_index =vcf_reader.infos['ANN'][3].split("|").index(" Gene_ID ")
 annotation_index = vcf_reader.infos['ANN'][3].split("|").index(" Annotation ")
 feature_id_index = vcf_reader.infos['ANN'][3].split("|").index(" Feature_ID ")
 
+#ka = number alleles with missense variants
 ka = 0
+# ks = number of alleles with synonymous variants
 ks = 0
+# total number of alleles for variant
 total = 0
-record = vcf_reader.next()
-while (int(record.POS) < 24852068) :
+
+
+#record = vcf_reader.next()
+#while (int(record.POS) < 24852068) :
+i = 1
+print "GeneID\tGeneName\tka\tks\tka_div_ks_plus1"
+for record in vcf_reader:
     curr_gene = str(record.INFO['ANN']).split('|')[geneid_index]
+    if i == 1:
+        prev_gene = curr_gene
+        i += 1    
+    if curr_gene != prev_gene:
+        print curr_gene + "\t" + str(record.INFO['ANN']).split('|')[gene_name_index] +"\t" + str(ka) +"\t" + str(ks) +"\t" + str( (ka) / float(ks+1))
+        #print "total " + str(total)
+        #make sure to reset ka, ks and total at each gene change
+        ka =0; ks =1; total =0
+    prev_gene = curr_gene
     for r in record.INFO['ANN']:
         #print( str(r).split('|')[geneid_index] +" " + str(r).split('|')[feature_id_index] +
         #      " " + str(r).split('|')[annotation_index] )
@@ -26,11 +52,4 @@ while (int(record.POS) < 24852068) :
             total += 2 * record.num_called
             #print ks
             
-    record= vcf_reader.next()
-    next_gene = str(record.INFO['ANN']).split('|')[geneid_index]
-    if curr_gene != next_gene:
-        print curr_gene + " " + str(record.INFO['ANN']).split('|')[gene_name_index]
-        print "ks " + str(ks)
-        print "ka " + str(ka)
-        print "total " + str(total)
-        ka =0; ks =0; total =0
+        
